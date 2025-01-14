43dcc84d213309fe526ed0dde109782d4ef47dc4
FelixTing <felix@iotechsys.com>
2025-01-14 16:26:07 +0800

    semver(main): 4.0.0-dev.1
    Signed-off-by: FelixTing <felix@iotechsys.com>

--- src/cmake/FindLIBCSDK.cmake
+++
@@ -1,4 +0,0 @@
-find_path (LIBCSDK_INCLUDE_DIR edgex/edgex.h edgex/devsdk.h)
-find_library (LIBCSDK_LIBRARIES NAMES csdk libcsdk)
-include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (LIBCSDK DEFAULT_MSG LIBCSDK_LIBRARIES LIBCSDK_INCLUDE_DIR)
